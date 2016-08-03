03c471b5e5529d27fb5253726de707d5ca304f7f
daniellasheva <daniellasheva@gmail.com>
2016-08-03 18:11:15 -0400

    initial commit

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,62 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song=self.new
+    @@all<<song
+    song
+  end
+
+  def self.new_by_name(name)
+    song=self.new
+    song.name=name
+    song
+  end
+
+  def self.create_by_name(name)
+    song=self.new
+    song.name=name
+    @@all<<song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect do |song|
+      song.name==name
+    end
+  end
+ 
+  def self.find_or_create_by_name(name)
+    if @@all.include?(name)
+      @@all.find {|song| song.name==name}
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename (filename)
+    array=filename.split(" - ")
+    song=new_by_name(name)
+    song.artist_name=array[0]
+    song.name= array[1].chomp(".mp3")
+    song
+  end
+
+  def self.create_from_filename(filename)
+    array=filename.split(" - ")
+    song=new_by_name(name)
+    song.artist_name=array[0]
+    song.name= array[1].chomp(".mp3")
+    @@all<<song
+    song
+  end
+
+  def self.destroy_all
+    @@all=[]
+  end
 end
+
